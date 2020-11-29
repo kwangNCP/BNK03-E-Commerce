95e484e8a45ccb25611c0623ac7bc4f768cdb73b
Natthapong Jiemjintanarom <natthapong41@gmail.com>
2020-11-29 11:03:44 +0700

    fix user interface

--- src/user/User.java
+++ src/user/User.java
@@ -1,7 +1,10 @@
 package user;
 
+import java.util.Scanner;
+
 public interface User {
-    public void run();
+
+    public void run(Scanner userInput);
     // private String add();
 
     // private String remove();
